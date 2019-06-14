5d7ce6dd955541c2661f1584abf6f6f9010f9379
HjortCraft <37836046+HjortCraft@users.noreply.github.com>
2019-06-14 18:40:53 +0200

    Add files via upload

---
+++ patch/js/firebase.js
@@ -0,0 +1,14 @@
+﻿var appFB = {};
+(function () {
+    // Make sure you replace this code with youre api key, that you get from firebase
+    var config = {
+        apiKey: "AIzaSyDsWeGkTPCwQdLrjmKvZ83c_LaY37IPtgc",
+        authDomain: "arveland-71eab.firebaseapp.com",
+        databaseURL: "https://arveland-71eab.firebaseio.com",
+        projectId: "arveland-71eab",
+        storageBucket: "arveland-71eab.appspot.com",
+        messagingSenderId: "759737890550",
+    };
+    firebase.initializeApp(config);
+    appFB = firebase;
+})();
---
+++ patch/js/main.js
@@ -0,0 +1,102 @@
+﻿var mainApp = {};
+(function(){
+    var currentUser = null;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          document.getElementById("login").style.display = "none";
+          document.getElementById("notloggedin_text").style.display = "none";
+          currentUser = user;
+        }else{
+            //location.replace("login.html");
+            document.getElementById("notloggedin_text").style.display = "block";
+            document.getElementById("text_verify").style.display = "none";
+            document.getElementById("verify_btn").style.display = "none";
+            document.getElementById("logout").style.display = "none";
+            document.getElementById("konto").style.display = "none";
+            document.getElementById("login").style.display = "block";
+        }
+      });
+    function fnLogout(){
+        firebase.auth().signOut();
+    }
+
+      function fnVerify(){
+       var user = firebase.auth().currentUser;
+        document.getElementById("verify").innerHTML =("Bekreftelse Link Er Sendt!");
+    
+      user.sendEmailVerification().then(function() {
+        // Email sent.
+      }).catch(function(error) {
+        // An error happened.
+      })
+    };
+
+    function fnDelete(){
+    var user = firebase.auth().currentUser;
+
+    user.delete().then(function() {
+      // User deleted.
+    }).catch(function(error) {
+      // An error happened.
+    });
+    }
+
+    function fnRepass(){
+
+        const email = firebase.auth().currentUser.email;
+        firebase.auth().sendPasswordResetEmail(email)
+          .then(function() {
+            document.getElementById("reset").innerHTML =("Tilbakestill Passord link er sendt.");
+          });
+        }
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+      
+          var user = firebase.auth().currentUser;
+      
+          if(user != null){
+      
+            var email_id = user.email;
+            var email_Verified = user.emailVerified;
+            var user = user.displayName;
+      
+            if(email_Verified){
+      
+              document.getElementById("verify_btn").style.display = "none";
+              document.getElementById("konto").style.display = "block";
+              document.getElementById("logout").style.display = "block";
+              document.getElementById("text_verify").style.display = "block";
+            } else {
+
+              document.getElementById("verify_btn").style.display = "block";
+              document.getElementById("logout").style.display = "blcok";
+              document.getElementById("konto").style.display = "none";
+              document.getElementById("text_verify").style.display = "none";
+            }
+            document.getElementById("user_para2").innerHTML =  " " + email_id;
+            document.getElementById("user_para").innerHTML =  "<br><br><br>E-Post : " + email_id + " <br><br>Bekreftet konto : " + email_Verified;
+          }
+
+        } else {
+          // No user is signed in.
+          
+        }
+      
+      }); 
+
+    function init(){
+        setTimeout(function(){
+        })
+        
+    }
+
+    init();
+
+      mainApp.repass = fnRepass;
+      mainApp.verify = fnVerify;
+      mainApp.logout = fnLogout;
+      mainApp.delete = fnDelete;
+})()
